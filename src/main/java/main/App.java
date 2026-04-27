6d4f8602e887037228250b94237d2e9c9f1e7c92
Kozinak <yroslavbot1@gmail.com>
2026-04-27 07:04:59 +0300

    Created main class

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,59 +1,69 @@
+package main;
+
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import main.Currency;
 
-void main() throws Exception {
-    Scanner sc = new Scanner(System.in);
-    String[] values = new String[] {"USD", "EUR", "CNY"};
-    List<Currency> currencies = reqestCurrency(values);
-    printCurrency(currencies);
-    boolean processed = true;
-    while (processed){
-        System.out.println("Введите 1 - Обновить данные | 0 - Выйти");
-        int userInput = sc.nextInt();
-        switch (userInput){
-            case 0:
-                processed = false;
-                break;
-            case 1:
-                System.out.print("\033[H\033[J");
-                printCurrency(currencies);
-                break;
+public class App {
+
+
+    private static List<Currency> currencies;
+
+    static void main() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String[] values = new String[]{"USD", "EUR", "CNY"};
+        currencies = reqestCurrency(values);
+        printCurrency();
+        boolean processed = true;
+        while (processed) {
+            System.out.println("Введите 1 - Обновить данные | 0 - Выйти");
+            int userInput = sc.nextInt();
+            switch (userInput) {
+                case 0:
+                    processed = false;
+                    break;
+                case 1:
+                    System.out.print("\033[H\033[J");
+                    printCurrency();
+                    break;
+            }
+        }
+
+
+    }
+
+    public static void printCurrency() {
+        for (Currency i : currencies) {
+            System.out.println(i);
         }
     }
 
+    public static List<Currency> reqestCurrency(String[] values) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+        currencies = new ArrayList<>();
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/66957f647ee253c5c43f65fa/latest/RUB"))
+                .timeout(java.time.Duration.ofSeconds(5))
+                .GET()
+                .build();
 
-}
-public void printCurrency(List<Currency> currencies){
-    for (Currency i : currencies){
-        System.out.println(i);
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonNode root = mapper.readTree(response.body());
+        for (String val : values) {
+            Currency cur = new Currency();
+            cur.createCurr(val, root.path("conversion_rates").path(val).asDouble());
+            currencies.add(cur);
+        }
+        return currencies;
     }
-}
-
-public List<Currency> reqestCurrency(String[] values)throws Exception {
-    HttpClient client = HttpClient.newHttpClient();
-    ObjectMapper mapper = new ObjectMapper();
-    List<Currency> currencies = new ArrayList<Currency>();
-
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://v6.exchangerate-api.com/v6/66957f647ee253c5c43f65fa/latest/RUB"))
-            .timeout(java.time.Duration.ofSeconds(5))
-            .GET()
-            .build();
-
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    JsonNode root = mapper.readTree(response.body());
-    for (int i = 0; i < values.length; i++){
-        Currency cur = new Currency();
-        cur.createCurr(values[i], root.path("conversion_rates").path(values[i]).asDouble());
-        currencies.add(cur);
-    }
-    return currencies;
 }
